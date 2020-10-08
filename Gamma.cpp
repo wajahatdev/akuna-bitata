bf5151193bbebe9f6d121ebac7df0da5758c2b91
Muhammad Wajahat Shahid <55183024+Wajishahid10@users.noreply.github.com>
2020-10-09 01:32:11 +0500

    Initialized Gamma

---
+++ Gamma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	
+	for (int i=0;i<5;i++)
+		cout<<"I love Pakistan."<<endl;	
+
+	bool isTodayFriday;
+	isTodayFriday= true;
+	if(isTodayFriday)
+	cout<<"Bhai Nahana Parey gaa";
+	else
+	cout<<"Khaire hi hia Cheetey";
+}
---
+++ Hacktoberfest.md
@@ -0,0 +1,2 @@
+This is soley for participation in Hacktoberfest 2020.
+Please avoid long scripts as if they are sort of typos.
